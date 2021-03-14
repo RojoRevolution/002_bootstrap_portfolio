73f8853e5e96eac20857bd069fe0aa3ecdd75b4e
David Rojo <rojo@rojorevolution.com>
2021-03-14 15:13:44 -0500

    deleted unneeded files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NoMatch from "./Pages/NoMatch";
-import Home from "./Pages/Home"
-import Work from "./Pages/Work"
-import About from "./Pages/About"
+import Home from "./pages/home"
+import Work from "./pages/Work"
+import About from "./pages/About"
 
 import NavBar from "./components/NavBar/"
 
@@ -17,9 +16,7 @@ function App() {
           <Route exact path="/home" component={Home} />
           <Route exact path="/work" component={Work} />
           <Route exact path="/about" component={About} />
-          <Route component={NoMatch} />
         </Switch>
-        {/* <Books /> */}
       </div>
     </Router>
   );
