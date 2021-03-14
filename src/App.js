5eeba8ff1cd189756aa983ba1da986fd94854a36
David Rojo <rojo@rojorevolution.com>
2021-03-13 19:14:14 -0600

    added react files and installed depedencies

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Dashboard from "./pages/Dashboard";
+import NoMatch from "./pages/NoMatch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// The app will not render correctly until you setup a Route component.
+// Refer to the Basic Example documentation if you need to.
+// (https://reacttraining.com/react-router/web/example/basic)
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route component={NoMatch} />
+        </Switch>
+        {/* <Books /> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
